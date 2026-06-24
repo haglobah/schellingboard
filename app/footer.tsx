a23efab49a97ffebf5afa7f5795bde05dee9bfe2
Beat Hagenlocher <bah@posteo.de>
2026-06-24 14:35:48 +0200

    fix: footer on mobile

--- app/footer.tsx
+++ app/footer.tsx
@@ -7,9 +7,12 @@ export default function Footer() {
   return (
     <footer className="lg:fixed bottom-0 left-0 right-0 bg-gray-50 border-t border-gray-200 py-2 z-20 mt-auto">
       <div className="px-3 flex justify-between items-center text-xs text-gray-500">
-        Version: {appVersion}
+        <span className="flex gap-1">
+          <span className="hidden sm:block">Version: </span>
+          {appVersion}
+        </span>
         <div className="flex items-center gap-1">
-          <span>Powered by</span>
+          <span className="hidden sm:block">Powered by</span>
           <a
             href="https://schellingboard.org"
             target="_blank"
